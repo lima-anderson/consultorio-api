0c595500818d31bca8b2a75beb8416ea842a6241
lima-anderson <msgli34@gmail.com>
2022-04-20 22:28:52 -0300

    iniciando o projeto

---
+++ src/main/java/com/deps/consultorioapi/ConsultorioApiApplication.java
@@ -0,0 +1,13 @@
+package com.deps.consultorioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deps/consultorioapi/ConsultorioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deps.consultorioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
