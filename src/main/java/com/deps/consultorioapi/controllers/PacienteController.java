a2b549b155cb4a2c091c46917a582c456f8f4cc7
lima-anderson <msgli34@gmail.com>
2022-04-23 12:00:57 -0300

    Crud Paciente + Exceções

--- src/main/java/com/deps/consultorioapi/controllers/PacienteController.java
+++ src/main/java/com/deps/consultorioapi/controllers/PacienteController.java
@@ -1,12 +1,16 @@
 package com.deps.consultorioapi.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.deps.consultorioapi.model.Paciente;
 import com.deps.consultorioapi.services.PacienteService;
@@ -14,14 +18,40 @@ import com.deps.consultorioapi.services.PacienteService;
 @RestController
 @RequestMapping(value = "/pacientes")
 public class PacienteController {
-	
+
 	@Autowired
 	private PacienteService paciente;
 
-	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<Paciente>> buscarTodos() {
 		List<Paciente> lista = paciente.buscarTodos();
 		return ResponseEntity.ok().body(lista);
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Paciente> buscarPorId(@PathVariable Long id) {
+		Paciente obj = paciente.buscarPorId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@RequestBody Paciente obj) {
+		obj = paciente.inserir(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> alterar(@PathVariable Long id, @RequestBody Paciente obj) {
+		obj.setId(id);
+		obj = paciente.alterar(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> apagar(@PathVariable Long id) {
+		paciente.apagar(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/com/deps/consultorioapi/services/PacienteService.java
+++ src/main/java/com/deps/consultorioapi/services/PacienteService.java
@@ -1,12 +1,14 @@
 package com.deps.consultorioapi.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.deps.consultorioapi.model.Paciente;
 import com.deps.consultorioapi.repositories.PacienteRepository;
+import com.deps.consultorioapi.services.excecoes.ObjetoNaoEncontradoException;
 
 @Service
 public class PacienteService {
@@ -16,6 +18,27 @@ public class PacienteService {
 
 	public List<Paciente> buscarTodos() {
 		return repo.findAll();
+	}
+	
+	public Paciente buscarPorId(Long id) {
+		Optional<Paciente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Paciente.class.getName()));
+	}
+		
+	public Paciente inserir(Paciente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Paciente alterar(Paciente obj) {
+		buscarPorId(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void apagar(Long id) {
+		buscarPorId(id);
+		repo.deleteById(id);
 	}
 
 }
