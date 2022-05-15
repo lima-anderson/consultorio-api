4df0929ff8a67043496153dd257198cc170c953c
lima-anderson <msgli34@gmail.com>
2022-05-15 17:16:46 -0300

    validações e correções

--- src/main/java/com/deps/consultorioapi/services/ConsultaService.java
+++ src/main/java/com/deps/consultorioapi/services/ConsultaService.java
@@ -2,11 +2,9 @@ package com.deps.consultorioapi.services;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.deps.consultorioapi.services.excecoes.ErroInternoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,88 +13,87 @@ import com.deps.consultorioapi.model.Medico;
 import com.deps.consultorioapi.model.Paciente;
 import com.deps.consultorioapi.model.DTO.ConsultaDTO;
 import com.deps.consultorioapi.repositories.ConsultaRepository;
-import com.deps.consultorioapi.services.excecoes.ObjetoNaoEncontradoException;
-
+import com.deps.consultorioapi.services.excecoes.ErroInternoException;
 
 @Service
 public class ConsultaService {
 
-    @Autowired
-    ConsultaRepository consultaRepository;
+	@Autowired
+	ConsultaRepository consultaRepository;
 
-    @Autowired
-    MedicoService medicoService;
+	@Autowired
+	MedicoService medicoService;
 
-    @Autowired
-    PacienteService pacienteService;
+	@Autowired
+	PacienteService pacienteService;
 
-    public List<Consulta> listarConsultas(){
-        return consultaRepository.findAll();
-    }
+	public List<Consulta> listarConsultas() {
+		return consultaRepository.findAll();
+	}
 
-    public Consulta buscarConsultaPorId(Long id){
-    	Optional<Consulta> obj = consultaRepository.findById(id);
+	public Consulta buscarConsultaPorId(Long id) {
+		Optional<Consulta> obj = consultaRepository.findById(id);
 		return obj.orElseThrow(() -> new ErroInternoException("Consulta Não encontrada"));
-    } 
-    
-    public Consulta criarConsulta(ConsultaDTO consultaDTO) throws ErroInternoException {
+	}
 
-        Medico medico = medicoService.buscarMedicoPorId(consultaDTO.getMedicoId());
-        Paciente paciente = pacienteService.buscarPorId(consultaDTO.getPacienteId());
+	public Consulta criarConsulta(ConsultaDTO consultaDTO) throws ErroInternoException {
 
-        if (consultaDTO.getDataConsulta().getDayOfWeek().equals(DayOfWeek.SATURDAY) || consultaDTO.getDataConsulta().getDayOfWeek().equals(DayOfWeek.SUNDAY)){
-            throw new ErroInternoException("AGENDAMENTOS APENAS DE SEGUNDA À SEXTA");
-        }
+		Medico medico = medicoService.buscarMedicoPorId(consultaDTO.getMedicoId());
+		Paciente paciente = pacienteService.buscarPorId(consultaDTO.getPacienteId());
 
-        List<Consulta> consultas = consultaRepository.findConsultaByDataConsultaAndMedico(consultaDTO.getDataConsulta(), medico);
+		if (consultaDTO.getDataConsulta().getDayOfWeek().equals(DayOfWeek.SATURDAY)
+				|| consultaDTO.getDataConsulta().getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+			throw new ErroInternoException("AGENDAMENTOS APENAS DE SEGUNDA À SEXTA");
+		}
 
-        if (consultas.size() >= 10){
-            throw new ErroInternoException("NÃO HÁ MAIS VAGAS PARA ESTE MÉDICO NESTA DATA");
-        }
+		List<Consulta> consultas = consultaRepository.findConsultaByDataConsultaAndMedico(consultaDTO.getDataConsulta(),
+				medico);
 
-        if (medico != null && paciente != null){
-            Consulta consulta = new Consulta(medico, paciente, consultaDTO.getDataConsulta());
-            medico.addConsulta(consulta);
-            paciente.addConsulta(consulta);
+		if (consultas.size() >= 10) {
+			throw new ErroInternoException("NÃO HÁ MAIS VAGAS PARA ESTE MÉDICO NESTA DATA");
+		}
+
+		if (medico != null && paciente != null) {
+			Consulta consulta = new Consulta(medico, paciente, consultaDTO.getDataConsulta());
+			medico.addConsulta(consulta);
+			paciente.addConsulta(consulta);
 
 //            medicoService.atualizarMedico(medico.getId(), medico);
 //            pacienteService.alterar(paciente);
 
-            return consultaRepository.save(consulta);
-        }
-        else {
-            throw new ErroInternoException("Medico ou paciente inválidos");
-        }
-    }
+			return consultaRepository.save(consulta);
+		} else {
+			throw new ErroInternoException("Medico ou paciente inválidos");
+		}
+	}
 
-    public void apagarConsulta(Long id) throws Exception {
+	public void apagarConsulta(Long id) throws Exception {
 
-        Consulta consulta = consultaRepository.findById(id).get();
-        if (consulta.getDataConsulta().isAfter(LocalDate.now())){
+		Consulta consulta = consultaRepository.findById(id).get();
+		if (consulta.getDataConsulta().isAfter(LocalDate.now())) {
 
-            consulta.getMedico().removerConsulta(consulta);
-            consulta.getPaciente().removerConsulta(consulta);
+			consulta.getMedico().removerConsulta(consulta);
+			consulta.getPaciente().removerConsulta(consulta);
 
-            consultaRepository.save(consulta);
-            consultaRepository.delete(consulta);
-        }else{
-            throw new Exception("Não é possível cancelar uma consulta passada!");
-        }
-    }
+			consultaRepository.save(consulta);
+			consultaRepository.delete(consulta);
+		} else {
+			throw new Exception("Não é possível cancelar uma consulta passada!");
+		}
+	}
 
-    public Consulta alterarConsulta(ConsultaDTO consultaDTO){
+	public Consulta alterarConsulta(ConsultaDTO consultaDTO) {
 
-        Consulta consulta = this.buscarConsultaPorId(consultaDTO.getIdConsulta());
-        Medico medico = medicoService.buscarMedicoPorId(consultaDTO.getMedicoId());
-        Paciente paciente = pacienteService.buscarPorId(consultaDTO.getPacienteId());
-        LocalDate dataConsulta = consultaDTO.getDataConsulta();
+		Consulta consulta = this.buscarConsultaPorId(consultaDTO.getIdConsulta());
+		Medico medico = medicoService.buscarMedicoPorId(consultaDTO.getMedicoId());
+		Paciente paciente = pacienteService.buscarPorId(consultaDTO.getPacienteId());
+		LocalDate dataConsulta = consultaDTO.getDataConsulta();
 
-        consulta.setMedico(medico);
-        consulta.setPaciente(paciente);
-        consulta.setDataConsulta(dataConsulta);
+		consulta.setMedico(medico);
+		consulta.setPaciente(paciente);
+		consulta.setDataConsulta(dataConsulta);
 
-        return consultaRepository.save(consulta);
-    }
-
+		return consultaRepository.save(consulta);
+	}
 
 }
--- src/main/java/com/deps/consultorioapi/services/excecoes/ErroInternoException.java
+++ src/main/java/com/deps/consultorioapi/services/excecoes/ErroInternoException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 public class ErroInternoException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
 
     public ErroInternoException(String mensagem){
         super(mensagem);
